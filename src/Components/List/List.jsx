0b3395547bbdb013deb371613418e17f441977c8
alireza <alirezaghasemi7777@gmail.com>
2022-08-05 16:40:37 +0430

    Add List and PlaceDetails

---
+++ src/Components/List/List.jsx
@@ -0,0 +1,55 @@
+import React, { useState, useEffect, createRef } from 'react';
+import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from '@material-ui/core';
+
+import PlaceDetails from '../PlaceDetails/PlaceDetails';
+import useStyles from './style.js';
+
+const List = () =>{
+    const classes = useStyles()
+    const [type,setType]= useState("resturant");
+    const [rate,setRate]= useState("0");
+    const plases = [
+        {name:"paasdsda"},
+        {name:"paasdsda"},
+        {name:"paasdsda"},
+        {name:"paasdsda"},
+        {name:"paasdsda"},
+        {name:"paasdsda"},
+        {name:"paasdsda"},
+        {name:"paasdsda"},
+        {name:"paasdsda"},
+        {name:"paasdsda"},
+        {name:"paasdsda"},
+    ]
+    return (
+        <div className={classes.container}>
+            <Typography variant="h4" className={classes.title}>
+            Food & Dining around you
+            </Typography>
+            <FormControl className={classes.formControl}>
+                <Select value={type} onChange={(e)=>setType(e.target.value)}>
+                    <MenuItem value="resturant">Resturant</MenuItem>
+                    <MenuItem value="hotels">Hotels</MenuItem>
+                    <MenuItem value="attractions">Attractions</MenuItem>
+                </Select>
+            </FormControl>
+            <FormControl className={classes.formControl}>
+                <Select value={rate} onChange={(e)=>setRate(e.target.value)}>
+                <MenuItem value="0">All</MenuItem>
+              <MenuItem value="3">Above 3.0</MenuItem>
+              <MenuItem value="4">Above 4.0</MenuItem>
+              <MenuItem value="4.5">Above 4.5</MenuItem>
+                </Select>
+            </FormControl>
+            <Grid container spacing={3} className={classes.list}>
+                {plases?.map((place, i)=>(
+                    <Grid item key={i} xs={12}>
+                        <PlaceDetails place={place}/>
+                    </Grid>
+                ))}
+            </Grid>
+        </div>
+    )
+}
+
+export default List
